14da268288850584f2dfc0861c7b60d857ff0f34
himiDias <himanshudias2004@gmail.com>
2025-06-18 18:15:48 +0100

    skeleton headr added for main element

---
+++ include/main_elem.h
@@ -0,0 +1,32 @@
+#ifndef MAIN_ELEMENT_H
+#define MAIN_ELEMENT_H
+
+#include "led-matrix.h"
+#include "graphics.h"
+
+#include "panel_element.h"
+
+#include <string>
+#include <vector>
+
+namespace desk_led{
+	class MainElement : public PanelElement {
+		public:
+		
+		MainElement(int x_lower, int x_upper, int y_lower, int y_upper);
+		
+		void drawOptions(rgb_matrix::FrameCanvas* canvas) const;
+		
+		void drawSelected(rgb_matrix::FrameCanvas* canvas) const;
+		
+		
+		private:
+
+		vector<PanelElement> options;
+		PanelElement selected;
+		
+	};
+	
+}
+
+#endif
