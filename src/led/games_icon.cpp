fd0c941df45cac905e87df0a27e405e74d33cdbf
himiDias <himanshudias2004@gmail.com>
2025-06-20 17:44:41 +0100

    set up games icon

---
+++ src/led/games_icon.cpp
@@ -0,0 +1,53 @@
+#include "led-matrix.h"
+#include "graphics.h"
+
+#include "games_icon.h"
+
+
+namespace desk_led{
+	IconGames::IconGames(int x_lower, int x_upper, int y_lower, int y_upper)
+	:IconElement(x_lower,x_upper,y_lower,y_upper)
+	{
+		
+	}
+	
+	IconGames::drawIcon(rgb_matrix::FrameCanvas* canvas){
+		check_canvas_null(canvas);
+		drawBorders(canvas);
+		int r,g,b;
+		// draw selection border
+		if (selected){
+			currentColor = &selectedColor;
+			r = currentColor->r;
+			g = currentColor->g;
+			b = currentColor->b;
+			for (int x=x_l+2;x<=x_u-2;++x){
+				canvas->SetPixel(x,y_l+2,r,g,b);
+				canvas->SetPixel(x,y_u-2,r,g,b);
+			}
+			
+			for (int y=y_l+2;y<y_u-2;++y){
+				canvas->SetPixel(x_l+2,y,r,g,b);
+				canvas->SetPixel(x_u-2,y,r,g,b);
+			}	
+		}else{
+			currentColor = &standardColor;
+			for (int x=x_l+2;x<=x_u-2;++x){
+				canvas->SetPixel(x,y_l+2,0,0,0);
+				canvas->SetPixel(x,y_u-2,0,0,0);
+			}
+			
+			for (int y=y_l+2;y<y_u-2;++y){
+				canvas->SetPixel(x_l+2,y,0,0,0);
+				canvas->SetPixel(x_u-2,y,0,0,0);
+			}
+		}
+		r = currentColor->r;
+		g = currentColor->g;
+		b = currentColor->b;
+		
+		int x_pos = (x_l + (x_u-x_l)/2);
+		int y_pos = (y_l + (y_u-y_l)/2);
+	}
+	
+}
