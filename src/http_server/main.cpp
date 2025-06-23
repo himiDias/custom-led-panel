a9d04df9da71ea79320972b9bd9ee0e4c2b51fba
himiDias <himanshudias2004@gmail.com>
2025-06-23 20:20:25 +0100

    template folder, and html working

--- src/http_server/main.cpp
+++ src/http_server/main.cpp
@@ -1,12 +1,22 @@
 #include "crow.h"
 
+#include <iostream>
+#include <string>
+
 int main()
 {
 	crow::SimpleApp app;
 	
+	std::cout << "Current workignn dir: " << std::filesystem::current_path() << std::endl;
+	//crow::mustache::set_base("src/http_server");
+ 
+	
 	CROW_ROUTE(app,"/")([](){
-		auto page = crow::mustache::load_text("/src/http_server/controller.html");
+		crow::mustache::set_base("src/http_server/templates");
+		auto page = crow::mustache::load_text("controller.html");
 		return page;
+		//return crow::mustache::load("controller.html").render();
+		//return "<html><body><p>TESTING IN LINE</p></body></html>";
 	});
 	
 	app.port(18080).multithreaded().run();
