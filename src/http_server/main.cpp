e687f275dc3fc2514df2f1c26a1a8b463163b240
himiDias <himanshudias2004@gmail.com>
2025-06-24 17:45:14 +0100

    started creating template

--- src/http_server/main.cpp
+++ src/http_server/main.cpp
@@ -15,8 +15,6 @@ int main()
 		crow::mustache::set_base("src/http_server/templates");
 		auto page = crow::mustache::load_text("controller.html");
 		return page;
-		//return crow::mustache::load("controller.html").render();
-		//return "<html><body><p>TESTING IN LINE</p></body></html>";
 	});
 	
 	app.port(18080).multithreaded().run();
